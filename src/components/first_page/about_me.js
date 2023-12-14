e7a234f4dcb54d7947d777d9e3ed0f842d3a832b
marieneoo <149239439+marieneoo@users.noreply.github.com>
2023-12-14 02:31:44 -0800

    responsive-first-page

--- src/components/first_page/about_me.js
+++ src/components/first_page/about_me.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import StarGreen from '../star_green'
+import Pic from '../../assets/images/pic.png'
 class AboutMe extends React.Component {
     render() {
         return(
@@ -16,6 +17,11 @@ class AboutMe extends React.Component {
                 <div className='hi'>
                     <p className='hi_txt'>Hi there! I'm Maria Tonoyan, a passionate 17-year-old graphic and web designer currently honing my skills at the Yerevan State College of Informatics. With a keen eye for aesthetics and a drive for innovation, I'm dedicated to crafting visually stunning designs that leave a lasting impression. </p>
                 </div>
+
+                <div className='hidden-pic-div'>
+                    <img className='hidden-pic' src={Pic}></img>  
+
+                    </div>
             </div>
         )
     }
