7b5084b49e6ce0e9aba7d597b09b6c2d96a23bbc
marieneoo <149239439+marieneoo@users.noreply.github.com>
2023-12-23 02:36:28 -0800

    css-start

--- src/components/portfolio/portfolio_carousel/illustrator_carousel.js
+++ src/components/portfolio/portfolio_carousel/illustrator_carousel.js
@@ -4,16 +4,22 @@ import geometric from "../../../assets/images/geometric.jpg"
 import march8 from "../../../assets/images/march8.jpg"
 import arrow_left from "../../../assets/images/arrow_left.svg"
 import arrow_right from "../../../assets/images/arrow_right.svg"
+import "../../../assets/styles/carousel.css"
 class PortfolioIllustratorCarousel extends React.Component {
     render() {
         return (
            <div className="portfolio_illustrator_carousel">
+
             <img src={arrow_left} className="arrow_left_img"></img>
+
+            <div className="carousel_images">
 
             <img src={stickers} className="stickers_img"></img>
             <img src={geometric} className="geometric_img"></img>
             <img src={march8} className="march8_img"></img>
-            
+
+            </div>
+
             <img src={arrow_right} className="arrow_right_img"></img>
 
 
